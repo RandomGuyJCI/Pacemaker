c8400763501d7e2b12c95359129835e89e4c5d95
RandomGuyJCI <carleisaguirre@gmail.com>
2022-09-05 12:30:30 +0800

    Add OptimizeTimelineResizing

---
+++ Pacemaker/Patches/OptimizeTimelineResizing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RDLevelEditor;
+
+namespace Pacemaker.Patches
+{
+    [HarmonyPatch]
+    public class OptimizeTimelineResizing
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(Timeline), "SetExpanded")]
+        [HarmonyPatch(typeof(Timeline), "ZoomVert")]
+        [HarmonyPatch(typeof(Timeline), "ZoomIn")]
+        [HarmonyPatch(typeof(Timeline), "ZoomOut")]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(false, 
+                    new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(Timeline), "UpdateUI", new []{ typeof(bool) })))
+                .Advance(-1)
+                .SetOpcodeAndAdvance(OpCodes.Ldc_I4_0)
+                .InstructionEnumeration();
+        }
+    }
+}
